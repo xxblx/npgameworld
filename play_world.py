7ffb405f3b989afd836aa7e80058055047516e04
Oleg Kozlov (xxblx) <xxblx.duke@gmail.com>
2017-08-06 14:01:40 +0300

    play_world upd: move hero into reversed direction from nearest enemy

--- play_world.py
+++ play_world.py
@@ -2,10 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from math import sqrt
 
 import pygame
 
 from npgameworld.world import World
+
+
+def get_hero_actions(enemies, hero_x, hero_y):
+    hero_actions = []
+
+    if not enemies:
+        return hero_actions
+
+    distances = []  # to enemies
+    for e in enemies:
+        t = (sqrt((e['x']-hero_x)**2 + (e['y']-hero_y)**2), e['enemy_id'])
+        distances.append(t)
+
+    nearest_enemy = sorted(distances)[0][1]
+    for e in enemies:
+        if e['enemy_id'] == nearest_enemy:
+            enemy_x = e['x']
+            enemy_y = e['y']
+
+    # Move into reversed direction from nearest enemy
+    # xd - x direction, yd - y direction
+    if enemy_x > hero_x:
+        xd = -1
+    elif enemy_x < hero_x:
+        xd = 1
+    else:
+        xd = 0
+
+    if enemy_y > hero_y:
+        yd = -1
+    elif enemy_y < hero_y:
+        yd = 1
+    else:
+        yd = 0
+
+    hero_actions.append({'cmd': 'move', 'xd': xd, 'yd': yd})
+
+    return hero_actions
 
 
 def main():
@@ -16,6 +55,7 @@ def main():
     world.add_enemy_type()
     world.logger.setLevel(logging.DEBUG)
 
+    # pygame used for circles drawing only
     screen = pygame.display.set_mode((world.screen_width, world.screen_height))
     clock = pygame.time.Clock()
     fps = 30
@@ -38,8 +78,12 @@ def main():
 
         screen.fill((0, 0, 0))
 
+        # Hero actions
+        hero_actions = get_hero_actions(world.world_stat['enemies'],
+                                        world.hero_x, world.hero_y)
+
         try:
-            wg.send(1)
+            wg.send(hero_actions)
         except StopIteration:
             break
 
