34cb7fd4957fe4c04b366bf2dce20f6d01779cb6
Oleg Kozlov (xxblx) <xxblx@posteo.org>
2017-08-15 23:19:05 +0300

    module docstring, version_info

--- npgameworld/__init__.py
+++ npgameworld/__init__.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+"""Simple pure python game engine
+
+NpGameWorld is very simple pure python game engine created for embedding.
+It designed for games like top-down shooters (crimsonlands, etc) where player
+controlls Hero by sending commands (as lists of dictionaries) to world.
+"""
+
+version_info = (0, 1, 0, 'dev0')
+__version__ = '.'.join(map(str, version_info))
